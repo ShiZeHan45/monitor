a828b335e5375be7a47f2771f8ab1947323e701d
SZH <szhsy1799@gmail.com>
2025-12-01 13:56:14 +0800

    SQL文件执行失败 不再从内存中读取，而是从H2中读取，程序重启才不影响SQL文件重试

--- src/main/java/com/szh/monitor/mapper/SqlExecuteLogMapper.java
+++ src/main/java/com/szh/monitor/mapper/SqlExecuteLogMapper.java
@@ -20,6 +20,6 @@ public interface SqlExecuteLogMapper extends BaseMapper<SqlExecuteLog> {
 
     @Select("SELECT failed_count FROM sql_execute_log WHERE environment_name=#{environmentName} and execute_date=#{date} and failed_count>0 order by failed_count desc limit 1")
     Integer findMaxFailedCount(String environmentName, Integer date);
-    @Update("UPDATE sql_execute_log SET failed_count=0,failed_count_reset_time=#{currTime} where environment_name=#{environmentName} and execute_date=#{date}")
+    @Update("UPDATE sql_execute_log SET failed_count=0,failed_count_reset_time=#{currTime} where environment_name=#{environmentName} and execute_date=#{date} and failed_count>0")
     void resetFailedCount(String environmentName, Integer date,Integer currTime);
 }
